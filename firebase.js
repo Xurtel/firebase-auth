d467bd4f85772c5a8c743542ccbc1be5ba4cf653
Matthew <mngor@ucsc.edu>
2019-05-09 18:27:51 -0700

    Reordered initialization of Firebase to fix bugs

--- firebase.js
+++ firebase.js
@@ -1,7 +1,12 @@
-//Paste the details given to you by firebase under this comment
-//eg
-// var firebaseConfig = {
-// ...config
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBM4yOM2UZQsxRtSKNDl5RnE-LpaCU81Bk",
+    authDomain: "fir-auth-example-2bd47.firebaseapp.com",
+    databaseURL: "https://fir-auth-example-2bd47.firebaseio.com",
+    projectId: "fir-auth-example-2bd47",
+    storageBucket: "fir-auth-example-2bd47.appspot.com",
+    messagingSenderId: "886124940752",
+    appId: "1:886124940752:web:e551efe649d20f00"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
